12a8471b0c6a438a8c8288ee61b8eb744be9e046
owaiskhan115 <73112225+owaiskhan115@users.noreply.github.com>
2021-02-07 15:30:18 +0500

    Program

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ListProject
+{
+    class Category
+    {
+
+        public string Name { get; set; }
+        public List<Category> Subs { get; set; }
+
+    }
+
+
+    class Program
+    {
+
+       List<string>  lines = System.IO.File.ReadAllLines(@"D:\categories.txt").ToList();
+
+
+
+        static void Main(string[] args)
+        {
+            //want to lines list object into category object. 
+            
+        }
+    }
+}
